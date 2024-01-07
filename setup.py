4d1cc8fcf6cd2bb774f6fdf0f3dc2d7c5b91ffce
DATUMBRIGHT <148606653+DATUMBRIGHT@users.noreply.github.com>
2024-01-07 12:20:36 -0500

    folder structure added

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 import setuptools
 
-with open(README.md,'r',encoding = "utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = '0.0.0'
+__version__ = "0.0.0"
 
-REPO_NAME = "cnnClassifier"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Chicken_Disease_Classification_Project"
+AUTHOR_USER_NAME = "DATUMBRIGHT"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "oforibright4u14@gmail.com"
 
-setuptools.setup(   
-    name = SRC_REPO,
-    version = __version__ ,
-    author = AUTHOR_USER_NAME,
-    author_email = AUTHOR_EMAIL,
-    description = "A small python package for CNN app"),
-    long_description_content = "text/markdown",
-    url = f"https://github.com{AUTHOR_USER_NAME}/{REPO_NAME}",
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Corrected argument name
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
